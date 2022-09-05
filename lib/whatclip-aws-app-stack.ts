703387052e0f81bf69d9d1e89a272c03a6427e03
ThatSameer <sameerhussain5@hotmail.com>
2022-09-05 17:16:53 +0100

    added eslint

--- lib/whatclip-aws-app-stack.ts
+++ lib/whatclip-aws-app-stack.ts
@@ -8,56 +8,57 @@ import { Construct } from 'constructs';
 import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 export class WhatclipAwsAppStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+	constructor(scope: Construct, id: string, props?: StackProps) {
+		super(scope, id, props);
 
-    const dynamoTable = new Table(this, 'whatclip-db', {
-      partitionKey: {
-        name: 'guildId',
-        type: AttributeType.STRING,
-      },
-      tableName: 'whatclip-db',
-    });
+		const dynamoTable = new Table(this, 'whatclip-db', {
+			partitionKey: {
+				name: 'guildId',
+				type: AttributeType.STRING,
+			},
+			tableName: 'whatclip-db',
+		});
 
-    const sqsQueue = new sqs.Queue(this, 'whatclip-sqs', {
-      queueName: 'whatclip-sqs',
-      visibilityTimeout: Duration.seconds(120),
-      retentionPeriod: Duration.days(1),
-    });
+		const sqsQueue = new sqs.Queue(this, 'whatclip-sqs', {
+			queueName: 'whatclip-sqs',
+			visibilityTimeout: Duration.seconds(120),
+			retentionPeriod: Duration.days(1),
+		});
 
-    const nodeJsFunctionProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: [
-          'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
-        ],
-      },
-      retryAttempts: 0,
-      timeout: Duration.seconds(90),
-      runtime: lambda.Runtime.NODEJS_16_X,
-    }
+		const nodeJsFunctionProps: NodejsFunctionProps = {
+			bundling: {
+				externalModules: [
+					'aws-sdk', // Use the 'aws-sdk' available in the Lambda runtime
+				],
+			},
+			retryAttempts: 0,
+			runtime: lambda.Runtime.NODEJS_16_X,
+		};
 
-    const sqsLambda = new NodejsFunction(this, 'whatclip-db-to-sqs', {
-      entry: path.join(__dirname, `../lambdas/sqsLambda.ts`),
-      environment: {
-        PRIMARY_KEY: 'guildId',
-        TABLE_NAME: dynamoTable.tableName,
-      },
-      ...nodeJsFunctionProps,
-    });
+		const sqsLambda = new NodejsFunction(this, 'whatclip-db-to-sqs', {
+			entry: path.join(__dirname, '../lambdas/sqsLambda.ts'),
+			timeout: Duration.seconds(60),
+			environment: {
+				PRIMARY_KEY: 'guildId',
+				TABLE_NAME: dynamoTable.tableName,
+			},
+			...nodeJsFunctionProps,
+		});
 
-    const discordLambda = new NodejsFunction(this, 'whatclip-sqs-to-discord', {
-      entry: path.join(__dirname, `../lambdas/discordLambda.ts`),
-      ...nodeJsFunctionProps,
-    });
+		dynamoTable.grantReadData(sqsLambda);
 
-    discordLambda.addEventSource(
-      new SqsEventSource(sqsQueue, {
-        batchSize: 1,
-      })
-    )
+		const discordLambda = new NodejsFunction(this, 'whatclip-sqs-to-discord', {
+			entry: path.join(__dirname, '../lambdas/discordLambda.ts'),
+			timeout: Duration.seconds(90),
+			...nodeJsFunctionProps,
+		});
 
-    dynamoTable.grantReadData(sqsLambda);
+		discordLambda.addEventSource(
+			new SqsEventSource(sqsQueue, {
+				batchSize: 1,
+			}),
+		);
 
 
-  }
+	}
 }
