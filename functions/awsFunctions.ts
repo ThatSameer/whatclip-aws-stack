e3099f7fd02e0a2d7b2676899b48f1c7227998ac
ThatSameer <sameerhussain5@hotmail.com>
2022-09-08 16:53:01 +0100

    code review commit

---
+++ functions/awsFunctions.ts
@@ -0,0 +1,46 @@
+import * as AWS from 'aws-sdk';
+
+const db = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS;
+
+export const scanTable = async (tableName: string) => {
+	try {
+		const params = {
+			TableName: tableName,
+			FilterExpression: 'active = :true AND isAutoClipOn = :true AND webhookId <> :null AND webhookToken <> :null AND streamerId <> :null',
+			ExpressionAttributeValues: {
+				':true': true,
+				':null': '',
+			},
+		};
+
+		const scanResults = [];
+		let items;
+		do {
+			items = await db.scan(params).promise();
+			items.Items.forEach((item) => scanResults.push(item));
+			params.ExclusiveStartKey = items.LastEvaluatedKey;
+		} while (typeof items.LastEvaluatedKey != 'undefined');
+
+		return scanResults;
+	}
+	catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
+
+export const sendSqs = async ({ messageBody, queueUrl }) => {
+	try {
+		const params = {
+			MessageBody: messageBody,
+			QueueUrl: queueUrl,
+		};
+
+		return await sqs.sendMessage(params).promise();
+	}
+	catch (error) {
+		console.log(error);
+		throw error;
+	}
+};
