3f61384443d8bf97dcf1d9d51d86a7f5dab83c12
ThatSameer <sameerhussain5@hotmail.com>
2022-09-08 16:51:32 +0100

    removal for code review

--- bin/whatclip-aws-app.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { WhatclipAwsAppStack } from '../lib/whatclip-aws-app-stack';
-
-const app = new cdk.App();
-new WhatclipAwsAppStack(app, 'WhatclipAwsAppStack');
